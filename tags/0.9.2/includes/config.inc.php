0a8942cafe74513b64e0cb59dd7deedfcbd6eec4
mikeyhoward1977 <mikeyhoward1977@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-11 00:27:04 +0000

    0.9.2 release
    git-svn-id: https://plugins.svn.wordpress.org/mobile-dj-manager@1023497 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.9.2/includes/config.inc.php
@@ -0,0 +1,11 @@
+<?
+/****************************************************************************************************
+--	Database information	--
+****************************************************************************************************/
+	$db_tbl = array(
+			'events'		=>	$wpdb->prefix . 'mdjm_events',
+			'playlists'	 =>	$wpdb->prefix . 'mdjm_playlists',
+			'journal'	   =>	$wpdb->prefix . 'mdjm_journal',
+			'venues'		=>	$wpdb->prefix . 'mdjm_venues'
+			);
+?>
